df8c61acf3dade72bf2a6e827bf2a34abf537fba
deakjahn <deakjahn@gmail.com>
2023-11-03 00:44:45 +0100

    More units.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [1.0.2] - 2023-11-03
+
+* More units.
+
 ## [1.0.1] - 2023-10-27
 
 * Fix SDK versions.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:textformfield_unit/textformfield_unit.dart';
 
-enum SizeUnits { Millimeter, Inch, LightNanosecond }
+enum SizeUnits { Millimeter, Inch, LightNanosecond, Twip, Thou, Barleycorn, Foot, Yard, Chain, Furlong, Mile, League, Fathom, NauticalMile, Link, Rod }
 
 void main() {
   runApp(const MyApp());
@@ -32,8 +32,21 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   static final unitNames = {
     SizeUnits.Millimeter: 'mm',
-    SizeUnits.Inch: '"',
+    SizeUnits.Inch: 'in',
     SizeUnits.LightNanosecond: 'light-ns',
+    SizeUnits.Twip: 'twip',
+    SizeUnits.Thou: 'th',
+    SizeUnits.Barleycorn: 'Bc',
+    SizeUnits.Foot: 'ft',
+    SizeUnits.Yard: 'yd',
+    SizeUnits.Chain: 'ch',
+    SizeUnits.Furlong: 'fur',
+    SizeUnits.Mile: 'mi',
+    SizeUnits.League: 'lea',
+    SizeUnits.Fathom: 'ftm',
+    SizeUnits.NauticalMile: 'nmi',
+    SizeUnits.Link: 'links',
+    SizeUnits.Rod: 'rods',
   };
   final formKey = GlobalKey<FormState>();
   late TextEditingController controller;
@@ -41,8 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    controller = TextEditingController(
-        text: NumberFormat.decimalPattern().format(156.25));
+    controller = TextEditingController(text: NumberFormat.decimalPattern().format(156.25));
   }
 
   @override
@@ -56,11 +68,37 @@ class _MyHomePageState extends State<MyHomePage> {
       SizeUnits.Millimeter => value,
       SizeUnits.Inch => value * 25.4,
       SizeUnits.LightNanosecond => value * 299.792458,
+      SizeUnits.Twip => value * 0.0176389,
+      SizeUnits.Thou => value * 0.0254,
+      SizeUnits.Barleycorn => value * 8.4667,
+      SizeUnits.Foot => value * 0304.8,
+      SizeUnits.Yard => value * 914.4,
+      SizeUnits.Chain => value * 20116.8,
+      SizeUnits.Furlong => value * 201168,
+      SizeUnits.Mile => value * 1609344,
+      SizeUnits.League => value * 4828032,
+      SizeUnits.Fathom => value * 1852,
+      SizeUnits.NauticalMile => value * 1852,
+      SizeUnits.Link => value * 0201.168,
+      SizeUnits.Rod => value * 5029.2,
     };
     return switch (toUnit) {
       SizeUnits.Millimeter => valueMm,
       SizeUnits.Inch => valueMm / 25.4,
       SizeUnits.LightNanosecond => valueMm / 299.792458,
+      SizeUnits.Twip => valueMm / 0.0176389,
+      SizeUnits.Thou => valueMm / 0.0254,
+      SizeUnits.Barleycorn => valueMm / 8.4667,
+      SizeUnits.Foot => valueMm / 0304.8,
+      SizeUnits.Yard => valueMm / 914.4,
+      SizeUnits.Chain => valueMm / 20116.8,
+      SizeUnits.Furlong => valueMm / 201168,
+      SizeUnits.Mile => valueMm / 1609344,
+      SizeUnits.League => valueMm / 4828032,
+      SizeUnits.Fathom => valueMm / 1852,
+      SizeUnits.NauticalMile => valueMm / 1852,
+      SizeUnits.Link => valueMm / 0201.168,
+      SizeUnits.Rod => valueMm / 5029.2,
     };
   }
 
@@ -87,9 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     TextFormUnitField<SizeUnits>(
                       controller: controller,
                       keyboardType: TextInputType.number,
-                      inputFormatters: [
-                        FilteringTextInputFormatter.allow(allowed)
-                      ],
+                      inputFormatters: [FilteringTextInputFormatter.allow(allowed)],
                       textInputAction: TextInputAction.next,
                       icon: const Icon(Icons.height),
                       labelText: 'Size',
@@ -98,8 +134,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       unitIcon: const Icon(Icons.keyboard_arrow_down),
                       converter: _sizeConverter,
                       onSaved: (value) {
-                        if (value != null)
-                          print('${value.value} ${unitNames[value.unit]}');
+                        if (value != null) print('${value.value} ${unitNames[value.unit]}');
                       },
                     ),
                   ],
